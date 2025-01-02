1cf017ec1fb8aa6a0ee20446b1083f6496a8a54d
alc_inf <alc_inf@yahoo.com.mx>
2025-01-02 16:50:22 -0600

    04. Change index with navvar

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,15 +7,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Starter</title>
+  <title>CRUD con Laravel 10</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet"
     href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
+  
   <!-- Theme style -->
-  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+  
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -26,21 +30,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <!-- Left navbar links -->
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+          <a class="nav-link" data-widget="pushmenu" href="{{ url('#') }}" role="button"><i class="fas fa-bars"></i></a>
         </li>
         <li class="nav-item d-none d-sm-inline-block">
-          <a href="/" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-          <a href="#" class="nav-link">Contact</a>
+          <a href="{{ url('/') }}" class="nav-link">CRUD con Laravel 10</a>
         </li>
       </ul>
 
       <!-- Right navbar links -->
       <ul class="navbar-nav ml-auto">
-        <!-- Navbar Search -->
+        <!-- Navbar Search 
         <li class="nav-item">
-          <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <a class="nav-link" data-widget="navbar-search" href="{{ url('#') }}" role="button">
             <i class="fas fa-search"></i>
           </a>
           <div class="navbar-search-block">
@@ -59,17 +60,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </form>
           </div>
         </li>
-        <!-- Messages Dropdown Menu -->
+        -->
+
+        <!-- Messages Dropdown Menu 
         <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
+          <a class="nav-link" data-toggle="dropdown" href="{{ url('#') }}">
             <i class="far fa-comments"></i>
             <span class="badge badge-danger navbar-badge">3</span>
           </a>
           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
+            <a href="{{ url('#') }}" class="dropdown-item">
+              <?php //Message Start ?>
               <div class="media">
-                <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+                <img src="{{ asset('dist/img/user1-128x128.jpg') }}" alt="User Avatar" class="img-size-50 mr-3 img-circle">
                 <div class="media-body">
                   <h3 class="dropdown-item-title">
                     Brad Diesel
@@ -79,13 +82,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                 </div>
               </div>
-              <!-- Message End -->
+              <?php //Message End ?>
             </a>
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
+            <a href="{{ url('#') }}" class="dropdown-item">
+              <?php //Message Start ?>
               <div class="media">
-                <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                <img src="{{ asset('dist/img/user8-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
                 <div class="media-body">
                   <h3 class="dropdown-item-title">
                     John Pierce
@@ -95,13 +98,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                 </div>
               </div>
-              <!-- Message End -->
+              <?php //Message End ?>
             </a>
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
-              <!-- Message Start -->
+            <a href="{{ url('#') }}" class="dropdown-item">
+              <?php //Message Start ?>
               <div class="media">
-                <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+                <img src="{{ asset('dist/img/user3-128x128.jpg') }}" alt="User Avatar" class="img-size-50 img-circle mr-3">
                 <div class="media-body">
                   <h3 class="dropdown-item-title">
                     Nora Silvester
@@ -111,54 +114,56 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
                 </div>
               </div>
-              <!-- Message End -->
+              <?php //Message End ?>
             </a>
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+            <a href="{{ url('#') }}" class="dropdown-item dropdown-footer">See All Messages</a>
           </div>
         </li>
+        -->
         
-        
-        <!-- Notifications Dropdown Menu -->
+        <!-- Notifications Dropdown Menu 
         <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
+          <a class="nav-link" data-toggle="dropdown" href="{{ url('#') }}">
             <i class="far fa-bell"></i>
             <span class="badge badge-warning navbar-badge">15</span>
           </a>
           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
             <span class="dropdown-header">15 Notifications</span>
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
+            <a href="{{ url('#') }}" class="dropdown-item">
               <i class="fas fa-envelope mr-2"></i> 4 new messages
               <span class="float-right text-muted text-sm">3 mins</span>
             </a>
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
+            <a href="{{ url('#') }}" class="dropdown-item">
               <i class="fas fa-users mr-2"></i> 8 friend requests
               <span class="float-right text-muted text-sm">12 hours</span>
             </a>
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item">
+            <a href="{{ url('#') }}" class="dropdown-item">
               <i class="fas fa-file mr-2"></i> 3 new reports
               <span class="float-right text-muted text-sm">2 days</span>
             </a>
             <div class="dropdown-divider"></div>
-            <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+            <a href="{{ url('#') }}" class="dropdown-item dropdown-footer">See All Notifications</a>
           </div>
         </li>
+        -->
+
         <li class="nav-item">
-          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <a class="nav-link" data-widget="fullscreen" href="{{ url('#') }}" role="button">
             <i class="fas fa-expand-arrows-alt"></i>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="{{ url('#') }}" role="button">
             <i class="fas fa-th-large"></i>
           </a>
         </li>
-        <!-- Logout Menu -->
+        <!-- Logout Menu 
         <li class="nav-item dropdown">
-          <a class="nav-link" data-toggle="dropdown" href="#">
+          <a class="nav-link" data-toggle="dropdown" href="{{ url('#') }}">
             {{ Auth::user()->name }}
           </a>
           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
@@ -171,6 +176,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </form>
           </div>
         </li>
+        -->
 
       </ul>
     </nav>
@@ -179,8 +185,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="/" class="brand-link">
-        <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <a href="{{ url('/') }}" class="brand-link">
+        <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
           style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
       </a>
@@ -190,22 +196,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
           <div class="image">
-            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
           </div>
           <div class="info">
-            <a href="#" class="d-block">Alexander Pierce</a>
-          </div>
-        </div>
-
-        <!-- SidebarSearch Form -->
-        <div class="form-inline">
-          <div class="input-group" data-widget="sidebar-search">
-            <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-            <div class="input-group-append">
-              <button class="btn btn-sidebar">
-                <i class="fas fa-search fa-fw"></i>
-              </button>
-            </div>
+            <a href="{{ url('#') }}" class="d-block">{{ Auth::user()->name }}</a>
           </div>
         </div>
 
@@ -214,8 +208,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
             <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-            <li class="nav-item menu-open">
-              <a href="#" class="nav-link active">
+            <li class="nav-item">
+              <a href="{{ url('#') }}" class="nav-link active">
                 <i class="nav-icon fas fa-tachometer-alt"></i>
                 <p>
                   Starter Pages
@@ -224,47 +218,31 @@ scratch. This page gets rid of all links and provides the needed markup only.
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="#" class="nav-link active">
+                  <a href="{{ url('#') }}" class="nav-link active">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Active Page</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{ url('#') }}" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Inactive Page</p>
                   </a>
                 </li>
               </ul>
             </li>
-            <li class="nav-item">
-              <a href="#" class="nav-link">
-                <i class="nav-icon fas fa-th"></i>
+            <li class="nav-item" >
+              <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();" style="background-color: #d82912">
+                <i class="nav-icon fas"><i class="bi bi-door-closed"></i></i>
                 <p>
-                  Simple Link
-                  <span class="right badge badge-danger">New</span>
+                  {{ __('Logout') }}
                 </p>
               </a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+              </form>
             </li>
-
-            <li class="nav-item menu-close">
-              <a href="#" class="nav-link active">
-                {{ Auth::user()->name }}
-                  <i class="right fas fa-angle-left"></i>
-              </a>
-              <ul class="nav nav-treeview">
-                <li class="nav-item">
-                  <a class="nav-link active" href="{{ route('logout') }}" onclick="event.preventDefault();
-                  document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                  </a>
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                  </form>
-                </li>
-              </ul>
-            </li>
-
           </ul>
         </nav>
         <!-- /.sidebar-menu -->
@@ -279,12 +257,12 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6">
-              <h1 class="m-0">Starter Page</h1>
+              <h1 class="m-0">Bienvenido al sistema</h1>
             </div><!-- /.col -->
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="#">Home</a></li>
-                <li class="breadcrumb-item active">Starter Page</li>
+                <li class="breadcrumb-item"><a href="{{ url('#') }}">Home</a></li>
+                <li class="breadcrumb-item active">Welcome</li>
               </ol>
             </div><!-- /.col -->
           </div><!-- /.row -->
@@ -307,8 +285,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     content.
                   </p>
 
-                  <a href="#" class="card-link">Card link</a>
-                  <a href="#" class="card-link">Another link</a>
+                  <a href="{{ url('#') }}" class="card-link">Card link</a>
+                  <a href="{{ url('#') }}" class="card-link">Another link</a>
                 </div>
               </div>
 
@@ -321,8 +299,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
                     card's
                     content.
                   </p>
-                  <a href="#" class="card-link">Card link</a>
-                  <a href="#" class="card-link">Another link</a>
+                  <a href="{{ url('#') }}" class="card-link">Card link</a>
+                  <a href="{{ url('#') }}" class="card-link">Another link</a>
                 </div>
               </div><!-- /.card -->
             </div>
@@ -337,14 +315,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                   <p class="card-text">With supporting text below as a natural lead-in to additional
                     content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <a href="{{ url('#') }}" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
 
 
 
               <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ url('#') }}" role="button" data-bs-toggle="dropdown"
                   aria-haspopup="true" aria-expanded="false" v-pre>
                   {{ Auth::user()->name }} ALC
                 </a>
@@ -370,7 +348,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
                   <p class="card-text">With supporting text below as a natural lead-in to additional
                     content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                  <a href="{{ url('#') }}" class="btn btn-primary">Go somewhere</a>
                 </div>
               </div>
             </div>
@@ -409,11 +387,11 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- REQUIRED SCRIPTS -->
 
   <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
+  <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
   <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
   <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
+  <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
 </body>
 
 </html>
